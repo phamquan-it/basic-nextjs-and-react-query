549eff722bf8d1a69064c540bc5215e48630aae2
SonTuyen123 <hanhngao3010@gmail.com>
2024-05-20 16:33:06 +0700

    init example

---
+++ src/pages/api/data.js
@@ -0,0 +1,28 @@
+const items = []
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req, res) => {
+  await new Promise((r) => setTimeout(r, 1000))
+
+  if (req.method === 'POST') {
+    const { text } = req.body
+
+    // sometimes it will fail, this will cause a regression on the UI
+
+    if (Math.random() > 0.7) {
+      res.status(500)
+      res.json({ message: 'Could not add item!' })
+      return
+    }
+
+    const newTodo = { id: Math.random().toString(), text: text.toUpperCase() }
+    items.push(newTodo)
+    res.json(newTodo)
+    return
+  } else {
+    res.json({
+      ts: Date.now(),
+      items,
+    })
+  }
+}
